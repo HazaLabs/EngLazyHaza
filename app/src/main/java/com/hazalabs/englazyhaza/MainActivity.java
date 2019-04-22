f969e23e8e4634df92c9e0514309c3d6577c5e75
Haza <e-primak@mail.ua>
2019-04-23 02:07:06 +0300

    Change design(ADD MENU)

--- app/src/main/java/com/hazalabs/englazyhaza/MainActivity.java
+++ app/src/main/java/com/hazalabs/englazyhaza/MainActivity.java
@@ -2,13 +2,38 @@ package com.hazalabs.englazyhaza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_settings:
+                //go on next activity
+                return true;
+            case R.id.action_about:
+                //go on next activity
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //testcodeand
+
     }
 }
